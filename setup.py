0d29a84ba749ecb3d3fb6aeccba7f1d60cd6be68
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-04-07 14:25:17 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="prophet-critique",
-    version="0.0.1",
+    version="0.0.2",
     description="Article list for Facebook prophet evaluation",
     long_description=README,
     long_description_content_type="text/markdown",
